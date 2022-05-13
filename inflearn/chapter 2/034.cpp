35090699906769b739d546ee58a7b617bf932b3a
unknown <hansungff3846@gmail.com>
2022-05-13 11:08:53 +0900

    버블정렬

--- inflearn/chapter 2/034.cpp
+++ inflearn/chapter 2/034.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	
-	int n;
+	int n, tmp;
 	cin >> n;
 	
 	int a[100];
@@ -14,7 +14,7 @@ int main(){
 	for(int i = 0; i < n-1; i++){
 		for(int j = 0; j < n-i; j++)
 			if(a[j] > a[j+1]){
-				int tmp = a[j];
+				tmp = a[j];
 				a[j] = a[j+1];
 				a[j+1] = tmp;
 			}
